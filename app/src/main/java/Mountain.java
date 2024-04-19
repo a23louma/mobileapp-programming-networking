d99fa7b2cc196d33c2f0cfa1872c3bf0ad7c8e19
a23louma <a23louma@student.his.se>
2024-04-19 15:00:03 +0200

    Created Mountain class

---
+++ app/src/main/java/Mountain.java
@@ -0,0 +1,18 @@
+
+public class Mountain {
+    private String name;
+    private String location;
+    private int height;
+
+    public Mountain(String name, String location, int height) {
+        this.name = name;
+        this.location = location;
+        this.height = height;
+    }
+
+    public Mountain(String name) {
+        this.name = name;
+        location = null;
+        height = 0;
+    }
+}
