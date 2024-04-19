95fb1175b66345ba5d8953f93236588143226828
a23louma <a23louma@student.his.se>
2024-04-19 15:08:22 +0200

    Added getters, setters and toString method in Mountain.java

--- app/src/main/java/Mountain.java
+++ app/src/main/java/Mountain.java
@@ -15,4 +15,35 @@ public class Mountain {
         location = null;
         height = 0;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Mountain{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
